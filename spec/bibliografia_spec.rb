2ca5689d17b8644e2aa4efffb50787e7df6720e8
rafael abadia <alu0100648782@ull.edu.es>
2015-11-04 17:30:09 +0000

    Creando Expectativa Instancia de clase Bibliografia

--- spec/bibliografia_spec.rb
+++ spec/bibliografia_spec.rb
@@ -3,11 +3,19 @@ require 'spec_helper'
 
 describe Libro do
 	  before(:all) do
+	    
+	    @b1 = Libro::Bibliografia.new({:autor =>['Dave Thomas', 'Andy Hunt', 'Chad Fowler'],:titulo => 'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide',:serie => 'The Facets of Ruby',:editorial => 'Pragmatic Bookshelf',:edicion => '4 edition',:fecha => 'July 7, 2013',:isbn => ['ISBN-13: 978-1937785499','ISBN-10: 1937785491']})
 		
 		
 	  end
 	  
 	  describe 'Expectativas clase Bibliografia' do
+	    
+	     it ' Expectativas instancia Bibliografia' do
+
+		    expect(@b1).to be_instance_of(Libro::Bibliografia)
+
+		  end
 
 
 	  end 
