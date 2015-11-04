1607621963147005cafa984c64e711a75c5da6ec
Juan José Gregorio Díaz Marrero <alu0100721805@ull.edu.es>
2015-11-04 17:38:33 +0000

    CUMPLIENDO Expectativa Debe existir uno o más autores

--- lib/bibliografia.rb
+++ lib/bibliografia.rb
@@ -5,6 +5,11 @@ module Libro
 		def initialize(texto)
 			 raise ArgumentError,'Argumento "texto" no debe ser nulo' unless texto != nil
 		 	 raise ArgumentError,'Argumento "texto" debe ser un Hash' unless texto.is_a? Hash
+
+			 raise ArgumentError,'Argumento "autor" no debe ser nulo' unless texto[:autor] != nil
+		  	 raise ArgumentError,'Argumento "autor" debe ser un Array' unless texto[:autor].is_a? Array
+		  	 texto[:autor].each {|x| raise ArgumentError,'El valor del array debe ser String' unless x.is_a? String}
+		  	 @autor = texto[:autor]
 		end
 	end
 
