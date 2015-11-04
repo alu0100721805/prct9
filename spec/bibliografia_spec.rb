7f0e5c2bdfa01559fced3f4b8bbd4af194085f15
rafael abadia <alu0100648782@ull.edu.es>
2015-11-04 17:36:32 +0000

    Creando Expectativa Debe existir uno o más autores

--- spec/bibliografia_spec.rb
+++ spec/bibliografia_spec.rb
@@ -15,7 +15,14 @@ describe Libro do
 
 		    expect(@b1).to be_instance_of(Libro::Bibliografia)
 
-		  end
+             end
+	     
+	     it ' Expectativa Debe existir uno o más autores' do
+
+		   expect{b2 = Libro::Bibliografia.new({:autor => nil,:titulo => 'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide',:serie => 'The Facets of Ruby',:editorial => 'Pragmatic Bookshelf',:edicion => '4 edition',:fecha => 'July 7, 2013',:isbn => ['ISBN-13: 978-1937785499','ISBN-10: 1937785491']})}.to raise_error(ArgumentError)
+
+	     end	
+		  
 
 
 	  end 
