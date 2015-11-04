5a9d1bd11eaf378ba3b084cd90ead56a96dee402
Juan José Gregorio Díaz Marrero <alu0100721805@ull.edu.es>
2015-11-04 17:33:51 +0000

    CUMPLIENDO Expectativa Instancia de clase Bibliografia

--- lib/bibliografia.rb
+++ lib/bibliografia.rb
@@ -2,7 +2,10 @@
 
 module Libro
  	class Bibliografia
-
+		def initialize(texto)
+			 raise ArgumentError,'Argumento "texto" no debe ser nulo' unless texto != nil
+		 	 raise ArgumentError,'Argumento "texto" debe ser un Hash' unless texto.is_a? Hash
+		end
 	end
 
 end
